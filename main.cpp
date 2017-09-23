d2dd9b8e03e76f0ee08a32fbed060cab0bee04a0
wangjiaming0909 <604227650@qq.com>
2017-09-23 14:29:57 +0800

    merge

--- main.cpp
+++ main.cpp
@@ -3,15 +3,9 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     main_ds1_21();
-=======
     // main_ds1_20();
->>>>>>> 5609a1f05a4738c17e41a86725c24fc8512875e2
-=======
     // main_ds1_20();
->>>>>>> 5609a1f05a4738c17e41a86725c24fc8512875e2
     // main_ds1_2();
     // main_multi_set_map();
     // main_insertsort();
