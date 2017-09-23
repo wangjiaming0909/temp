cbe5b88d8d4ae90cc2e2f3d9794ed506587cdb5d
wangjiaming0909 <604227650@qq.com>
2017-09-23 13:07:07 +0800

    insert_sort

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-    main_ds1_20();
+    // main_ds1_20();
     // main_ds1_2();
     // main_multi_set_map();
-    // main_insertsort();
+    main_insertsort();
     // main_quickfind();
-    cout << "asd" << endl;
+    // cout << "asd" << endl;
     return 0;
 }
--- main_functions.h
+++ main_functions.h
@@ -8,7 +8,7 @@ void main_insertsort()
     using algorithms::insert_sort;
     int nums[10] = {2, 3, 1, 62, 12, 7, 95, 21, 6, 33};
     // int nums[4] = {2,3,1,0};
-    int *p = insert_sort(nums, 10);
+    int *p = algorithms::insert_sort2(nums, 10);
     for (int i = 0; i < 10; i++)
     {
         cout << *(p++) << " ";
