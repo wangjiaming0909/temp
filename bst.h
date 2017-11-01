82e538802616ea0db3f841a2c1add73faca72c79
wangjiaming0909 <604227650@qq.com>
2017-11-01 11:36:52 +0800

    search

--- bst.h
+++ bst.h
@@ -7,7 +7,7 @@ template <typename T, typename V>
 class BST : public ds::linkedBinaryTree<pair<T, V>>{
 public:
     BST(binaryTreeNode<pair<T, V>> *r) : linkedBinaryTree<pair<T, V>>(r){}
-    virtual binaryTreeNode<pair<T, V>> *search(const T&, bool b= false) const;
+    virtual binaryTreeNode<pair<T, V>> *search(const T&) const;
     virtual binaryTreeNode<pair<T, V>> *insert(const pair<T, V>&);
     // virtual bool remove(const T&);
 protected:    
@@ -17,34 +17,42 @@ protected:
         binaryTreeNode<pair<T, V>>*
     );
     binaryTreeNode<pair<T, V>>* rotateAt(binaryTreeNode<pair<T, V>> *x);
-    // binaryTreeNode<pair<T, V>>* _hot;
+    binaryTreeNode<pair<T, V>>* _hot;
 private:
+    // pair<binaryTreeNode<pair<T, V>>, bool> search(const pair<T, V> &p);
 };
 
+// template <typename T, typename V>
+// pair<binaryTreeNode<pair<T, V>>, bool> BST<T, V>::search(const pair<T, V> &p){
+//     auto t = search(p.first);
+// }
 template <typename T, typename V>
-binaryTreeNode<pair<T, V>>* BST<T, V>::search(const T& t, bool b = false) const{
-    if(!b){
-        binaryTreeNode<pair<T, V>> *p = this->root;
-        while(p != NULL){
-            if(t < p->element.first)
+binaryTreeNode<pair<T, V>>* BST<T, V>::search(const T& t) const{
+    binaryTreeNode<pair<T, V>> *p = this->root;
+    while(p != NULL){
+        if(t < p->element.first)
             p = p->leftChild;
-            else
+        else
             if(t > p->element.first)
-            p = p->rightChild;
+                p = p->rightChild;
             else
-            return p;
-        }
-        return NULL;
+                return p;
     }
+    _hot = const_cast<binaryTreeNode<pair<T, V>>*>(p);
+    return NULL;
 }
-
+    
 template <typename T, typename V>
 binaryTreeNode<pair<T, V>>* BST<T,V>::insert(const pair<T, V>& t){
-    auto p = search(t);
+    auto p = search(t.first);
     if(p) {
         p->element.second = t.second;
         return p;
     }
     p = new binaryTreeNode<pair<T, V>>(t, nullptr, nullptr);
+    if(t.first < _hot->element.first)
+        _hot->leftChild = p;
+    else
+        _hot->rightChild = p;
 }
 #endif // _BST_H_
