b6a9fb42158e6e81690c37aa4528836c2b2c7ee5
Jiaming Wang <jwang284@slb.com>
2018-12-17 15:17:35 +0800

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,9 @@
-// #include "main_functions.h"
-// #include "stl.h"
-#include <vector>
-#include <queue>
-#include <string>
-#include <sstream>
-// #include "boost_test.h"
 #include "al/leetcode_headers.h"
 using namespace std;
 
 int main()
 {
-    // main_redblack();
-    // main_graph();
-    // main_graph2();
-    // main_digraph();
-    // main_deque();
-    // main_count_if();
-    // main_bind();
-    // main_set();
-    // main_multi_set_map();
-    // main_type_traits();
-    // main_map();
-    // main_hash_set_map();
-    // main_stl_algorithms();
-    // main_cpp_();
-    // main_opt_new();
-    // cout << 123 << endl;
-    // main_hello();
-    // main_timer();
-    // main_multi_param_of_template_param();
-
     // al::LongestSubString().test();
     al::Median_of_two_stored_vector().test();
     // al::Longest_palindromic_strstring().test();
-
 }
-
-//bb2baeeec14e0ff29b4035f2dea60245e338fcc3
