0fe7179866170d760b6998cb5bf3918d3f02fbc4
wangjiaming0909 <604227650@qq.com>
2019-12-02 20:17:49 +0800

    300 leetcode n2

--- src/al/300lengthoflis.h
+++ src/al/300lengthoflis.h
@@ -1,5 +1,6 @@
 #include "leetcode.h"
-#include <queue>
+#include <algorithm>
+#include <cstdint>
 
 namespace al {
 
@@ -7,22 +8,37 @@ class LengthOfLIS : public ILeetCode
 {
 
 public:
+    int findMaxBefore(vector<int>& nums, vector<int>& v, int i)
+    {
+        int max = INT32_MIN;
+        for(int j = 0; j < i; j++)
+        {
+            if(nums[j] >= nums[i]) continue;
+            max = std::max(max, v[j]);
+        }
+        if(max == INT32_MIN) return 0;
+        return max;
+    }
 
     int lengthOfLIS(vector<int>& nums)
     {
-        priority_queue<int> maxHeap{};
-        for(auto i : nums)
+        vector<int> v;
+        v.resize(nums.size());
+
+        for(int i = 0; i < nums.size(); i++)
         {
-
-
+            auto val = findMaxBefore(nums, v, i);
+            v[i] = val+1;
         }
 
-
+        return *std::max_element(v.begin(), v.end());
     }
 
     void test() override
     {
-
+//        [10,9,2,5,3,7,101,18]
+        vector<int> v = {10, 9, 2, 5, 3, 7, 101, 18};
+        cout << lengthOfLIS(v) << endl;
     }
 };
 }
